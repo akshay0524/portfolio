f67bf65652c745709e0c46de6546cbbbd45d0790
unknown <akshay246908@gmail.com>
2026-01-17 00:21:07 +0530

    Updates

--- assets/Contact-DDKb6Lw-.js
+++ assets/Contact-DDKb6Lw-.js
@@ -1,4 +1,4 @@
-import{c as u,r,j as e}from"./index-DifrzPTe.js";const g=[["path",{d:"M14.536 21.686a.5.5 0 0 0 .937-.024l6.5-19a.496.496 0 0 0-.635-.635l-19 6.5a.5.5 0 0 0-.024.937l7.93 3.18a2 2 0 0 1 1.112 1.11z",key:"1ffxy3"}],["path",{d:"m21.854 2.147-10.94 10.939",key:"12cjpa"}]],x=u("send",g),f=()=>{const[a,s]=r.useState({name:"",email:"",message:""}),[l,n]=r.useState(!1),[d,o]=r.useState(!1),i=t=>{t.preventDefault(),n(!0);const c=encodeURIComponent(`Portfolio Contact from ${a.name}`),m=encodeURIComponent(`Name: ${a.name}
+import{c as u,r,j as e}from"./index-CZIakAVi.js";const g=[["path",{d:"M14.536 21.686a.5.5 0 0 0 .937-.024l6.5-19a.496.496 0 0 0-.635-.635l-19 6.5a.5.5 0 0 0-.024.937l7.93 3.18a2 2 0 0 1 1.112 1.11z",key:"1ffxy3"}],["path",{d:"m21.854 2.147-10.94 10.939",key:"12cjpa"}]],x=u("send",g),f=()=>{const[a,s]=r.useState({name:"",email:"",message:""}),[l,n]=r.useState(!1),[d,o]=r.useState(!1),i=t=>{t.preventDefault(),n(!0);const c=encodeURIComponent(`Portfolio Contact from ${a.name}`),m=encodeURIComponent(`Name: ${a.name}
 Email: ${a.email}
 
 Message:
